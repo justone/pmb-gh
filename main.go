bd10e8a7105f1fef9048c92c03d95001a36ab82c
Nate Jones <nate@endot.org>
2015-12-27 21:57:29 -0800

    small notification test

---
+++ main.go
@@ -0,0 +1,22 @@
+package main
+
+import (
+	"fmt"
+	"os"
+
+	"github.com/justone/pmb/api"
+)
+
+func main() {
+
+	bus := pmb.GetPMB(make(map[string]string))
+	id := pmb.GenerateRandomID("notify")
+
+	conn, err := bus.ConnectClient(id, false)
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
+
+	pmb.SendNotificationWithLevel(conn, "Test message", 3)
+}
