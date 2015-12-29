bd34a39ef76e93e798f21907a9a0a648df98bec7
Nate Jones <nate@endot.org>
2015-12-28 20:19:24 -0800

    set up web service and print incoming body

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 	"os"
 
 	"github.com/justone/pmb/api"
@@ -18,5 +20,16 @@ func main() {
 		os.Exit(1)
 	}
 
-	pmb.SendNotificationWithLevel(conn, "Test message", 3)
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		body, err := ioutil.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, "Unable to read request body: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		fmt.Println(string(body))
+		go func() {
+			pmb.SendNotificationWithLevel(conn, "Test message", 3)
+		}()
+	})
+	http.ListenAndServe(":3000", nil)
 }
