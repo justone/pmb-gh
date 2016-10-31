bf70ec419bc80e75e19ff3c569eaa65c94a037b4
Nate Jones <nate@endot.org>
2016-10-31 16:56:59 -0400

    add ability to ignore users

--- main.go
+++ main.go
@@ -13,11 +13,12 @@ import (
 )
 
 var opts struct {
-	Verbose bool    `short:"v" long:"verbose" description:"Show verbose debug information."`
-	Primary string  `short:"m" long:"pmb-uri" description:"Primary PMB URI."`
-	Level   float64 `short:"l" long:"level" description:"Level at which to send notifications." default:"4"`
-	Host    string  `short:"h" long:"host" description:"Host to listen on." default:"0.0.0.0"`
-	Port    string  `short:"p" long:"port" description:"Port to listen on." default:"3000"`
+	Verbose bool     `short:"v" long:"verbose" description:"Show verbose debug information."`
+	Primary string   `short:"m" long:"pmb-uri" description:"Primary PMB URI."`
+	Ignore  []string `short:"i" long:"ignore" description:"Github username to ignore."`
+	Level   float64  `short:"l" long:"level" description:"Level at which to send notifications." default:"4"`
+	Host    string   `short:"h" long:"host" description:"Host to listen on." default:"0.0.0.0"`
+	Port    string   `short:"p" long:"port" description:"Port to listen on." default:"3000"`
 }
 
 func main() {
@@ -25,6 +26,11 @@ func main() {
 	_, err := flags.Parse(&opts)
 	if err != nil {
 		os.Exit(1)
+	}
+
+	ignoreUsers := make(map[string]bool)
+	for _, u := range opts.Ignore {
+		ignoreUsers[u] = true
 	}
 
 	bus := pmb.GetPMB(opts.Primary)
@@ -67,7 +73,12 @@ func main() {
 
 		eventJSON := string(body)
 
-		notification, err := parseEvent(eventName, eventJSON)
+		notification, login, err := parseEvent(eventName, eventJSON)
+
+		if _, ok := ignoreUsers[login]; ok {
+			logrus.Warnf(fmt.Sprintf("ignoring notification from %s", login))
+			return
+		}
 		if err != nil {
 			logrus.Warnf("Unable to parse event %s: %s, body: %s", eventName, err, eventJSON)
 			return
--- message.go
+++ message.go
@@ -9,7 +9,7 @@ import (
 	"github.com/justone/pmb/api"
 )
 
-func parseEvent(name string, json string) (*pmb.Notification, error) {
+func parseEvent(name string, json string) (*pmb.Notification, string, error) {
 
 	var message string
 	var url string
@@ -17,16 +17,16 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 	tree := jsontree.New()
 	err := tree.UnmarshalJSON([]byte(json))
 	if err != nil {
-		return nil, err
+		return nil, "", err
 	}
 
 	repo, err := tree.Get("repository").Get("full_name").String()
 	if err != nil {
-		return nil, fmt.Errorf("Unable to get full name of repository: %s", err)
+		return nil, "", fmt.Errorf("Unable to get full name of repository: %s", err)
 	}
 	login, err := tree.Get("sender").Get("login").String()
 	if err != nil {
-		return nil, fmt.Errorf("Unable to get full name of repository: %s", err)
+		return nil, "", fmt.Errorf("Unable to get full name of repository: %s", err)
 	}
 
 	skip := false
@@ -35,26 +35,26 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 	case "create":
 		ref_type, err := tree.Get("ref_type").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get ref_type: %s", err)
+			return nil, "", fmt.Errorf("Unable to get ref_type: %s", err)
 		}
 		ref, err := tree.Get("ref").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get ref: %s", err)
+			return nil, "", fmt.Errorf("Unable to get ref: %s", err)
 		}
 		message = fmt.Sprintf("New %s (%s) for %s by %s.", ref_type, ref, repo, login)
 		base_url, err := tree.Get("repository").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 		url = fmt.Sprintf("%s/tree/%s", base_url, ref)
 	case "delete":
 		ref_type, err := tree.Get("ref_type").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get ref_type: %s", err)
+			return nil, "", fmt.Errorf("Unable to get ref_type: %s", err)
 		}
 		ref, err := tree.Get("ref").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get ref: %s", err)
+			return nil, "", fmt.Errorf("Unable to get ref: %s", err)
 		}
 		if strings.HasPrefix(ref, "refs/heads/") {
 			ref = strings.TrimPrefix(ref, "refs/heads/")
@@ -62,25 +62,25 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 		message = fmt.Sprintf("Delete %s (%s) for %s by %s.", ref_type, ref, repo, login)
 		base_url, err := tree.Get("repository").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 		url = fmt.Sprintf("%s/tree/%s", base_url, ref)
 	case "watch":
 		message = fmt.Sprintf("New star for %s by %s.", repo, login)
 		url, err = tree.Get("sender").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "fork":
 		message = fmt.Sprintf("New fork for %s by %s.", repo, login)
 		url, err = tree.Get("forkee").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "push":
 		commits, err := tree.Get("commits").Len()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get commits: %s", err)
+			return nil, "", fmt.Errorf("Unable to get commits: %s", err)
 		}
 		// skip notification this is new, the create event will suffice
 		if commits == 0 {
@@ -88,7 +88,7 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 		}
 		ref, err := tree.Get("ref").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get ref: %s", err)
+			return nil, "", fmt.Errorf("Unable to get ref: %s", err)
 		}
 		if strings.HasPrefix(ref, "refs/heads/") {
 			ref = strings.TrimPrefix(ref, "refs/heads/")
@@ -96,24 +96,24 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 		message = fmt.Sprintf("Push %d commit(s) to %s in %s by %s.", commits, ref, repo, login)
 		url, err = tree.Get("compare").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "pull_request":
 		action, err := tree.Get("action").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get action: %s", err)
+			return nil, "", fmt.Errorf("Unable to get action: %s", err)
 		}
 		issue, err := tree.Get("pull_request").Get("number").Number()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request number: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request number: %s", err)
 		}
 		title, err := tree.Get("pull_request").Get("title").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request title: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request title: %s", err)
 		}
 		body, err := tree.Get("pull_request").Get("body").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get body: %s", err)
+			return nil, "", fmt.Errorf("Unable to get body: %s", err)
 		}
 		message = fmt.Sprintf(
 			"Pull request %s #%d (%s) on %s by %s: %s",
@@ -125,24 +125,24 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 			truncate(body, 40))
 		url, err = tree.Get("pull_request").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "issue_comment":
 		action, err := tree.Get("action").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get action: %s", err)
+			return nil, "", fmt.Errorf("Unable to get action: %s", err)
 		}
 		issue, err := tree.Get("issue").Get("number").Number()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get issue number: %s", err)
+			return nil, "", fmt.Errorf("Unable to get issue number: %s", err)
 		}
 		issue_title, err := tree.Get("issue").Get("title").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get issue title: %s", err)
+			return nil, "", fmt.Errorf("Unable to get issue title: %s", err)
 		}
 		body, err := tree.Get("comment").Get("body").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get body: %s", err)
+			return nil, "", fmt.Errorf("Unable to get body: %s", err)
 		}
 		message = fmt.Sprintf(
 			"Comment %s on issue %d (%s) on %s by %s: %s",
@@ -154,28 +154,28 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 			truncate(body, 40))
 		url, err = tree.Get("comment").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "pull_request_review":
 		action, err := tree.Get("action").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get action: %s", err)
+			return nil, "", fmt.Errorf("Unable to get action: %s", err)
 		}
 		state, err := tree.Get("review").Get("state").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get state: %s", err)
+			return nil, "", fmt.Errorf("Unable to get state: %s", err)
 		}
 		issue, err := tree.Get("pull_request").Get("number").Number()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request number: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request number: %s", err)
 		}
 		title, err := tree.Get("pull_request").Get("title").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request title: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request title: %s", err)
 		}
 		body, err := tree.Get("review").Get("body").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get body: %s", err)
+			return nil, "", fmt.Errorf("Unable to get body: %s", err)
 		}
 		message = fmt.Sprintf(
 			"PR Review %s (%s) on issue %d (%s) on %s by %s: %s",
@@ -188,24 +188,24 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 			truncate(body, 40))
 		url, err = tree.Get("review").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "pull_request_review_comment":
 		action, err := tree.Get("action").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get action: %s", err)
+			return nil, "", fmt.Errorf("Unable to get action: %s", err)
 		}
 		issue, err := tree.Get("pull_request").Get("number").Number()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request number: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request number: %s", err)
 		}
 		title, err := tree.Get("pull_request").Get("title").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get pull_request title: %s", err)
+			return nil, "", fmt.Errorf("Unable to get pull_request title: %s", err)
 		}
 		body, err := tree.Get("comment").Get("body").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get body: %s", err)
+			return nil, "", fmt.Errorf("Unable to get body: %s", err)
 		}
 		message = fmt.Sprintf(
 			"PR Comment %s on issue %d (%s) on %s by %s: %s",
@@ -217,20 +217,20 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 			truncate(body, 40))
 		url, err = tree.Get("comment").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "issues":
 		action, err := tree.Get("action").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get action: %s", err)
+			return nil, "", fmt.Errorf("Unable to get action: %s", err)
 		}
 		issue, err := tree.Get("issue").Get("number").Number()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get issue number: %s", err)
+			return nil, "", fmt.Errorf("Unable to get issue number: %s", err)
 		}
 		issue_title, err := tree.Get("issue").Get("title").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get issue title: %s", err)
+			return nil, "", fmt.Errorf("Unable to get issue title: %s", err)
 		}
 		message = fmt.Sprintf(
 			"Issue %d (%s) %s on %s by %s.",
@@ -241,18 +241,18 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 			login)
 		url, err = tree.Get("issue").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	case "ping":
 		zen, err := tree.Get("zen").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get zen: %s", err)
+			return nil, "", fmt.Errorf("Unable to get zen: %s", err)
 		}
 
 		message = fmt.Sprintf("Ping for %s by %s. Zen: %s", repo, login, zen)
 		url, err = tree.Get("repository").Get("html_url").String()
 		if err != nil {
-			return nil, fmt.Errorf("Unable to get url: %s", err)
+			return nil, "", fmt.Errorf("Unable to get url: %s", err)
 		}
 	default:
 		message = fmt.Sprintf("Unhandled event %s for %s by %s.", name, repo, login)
@@ -263,9 +263,9 @@ func parseEvent(name string, json string) (*pmb.Notification, error) {
 	logrus.Debugf("url: %s", url)
 
 	if skip {
-		return nil, nil
+		return nil, "", nil
 	}
-	return &pmb.Notification{Message: message, URL: url}, nil
+	return &pmb.Notification{Message: message, URL: url}, login, nil
 }
 
 func truncate(data string, length int) string {
